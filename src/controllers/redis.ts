605a056ab8301581767089c6e821525ecf198489
Ungs <iqeq1945@gmail.com>
2024-01-16 13:12:28 +0900

    🚧 redis 관련 수정 및 최근 검색기능 추가

--- src/controllers/redis.ts
+++ src/controllers/redis.ts
@@ -3,10 +3,4 @@ import express, { Request, Response } from 'express';
 
 const Router = express.Router();
 
-Router.get('/', async (req: Request, res: Response) => {
-  const response = await RedisRepository.setTest();
-  console.log(response, 'hihi');
-  res.send(response);
-});
-
 export default Router;
--- src/repositories/RedisRepository.ts
+++ src/repositories/RedisRepository.ts
@@ -1,11 +1,16 @@
 import { redisCli } from '../config/redis';
 
-export const setTest = async () => {
+export const setKeyword = async (keyword: string, id: number) => {
   try {
-    await redisCli.set('keyword', 'val');
-    const response = await redisCli.get('keyword');
-    console.log(response);
-    return response;
+    return await redisCli.zAdd(id, [{ score: Date.now(), value: keyword }]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getKeyword = async (id: number) => {
+  try {
+    return await redisCli.zRange(id, -5, -1);
   } catch (err) {
     console.log(err);
   }
