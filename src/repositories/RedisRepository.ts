a091dd89e3520d35612e17b9ade32bf726eccc23
Ungs <iqeq1945@gmail.com>
2024-02-20 12:09:03 +0900

    ⚡️ Redis 관련 Repository 함수 구현

--- src/repositories/RedisRepository.ts
+++ src/repositories/RedisRepository.ts
@@ -2,7 +2,7 @@ import { redisCli } from '../config/redis';
 
 export const setKeyword = async (keyword: string, id: number) => {
   try {
-    return await redisCli.zAdd(`Keyword:${id}`, [
+    return await redisCli.zAdd(`keyword:${id}`, [
       { score: Date.now(), value: keyword },
     ]);
   } catch (err) {
@@ -12,7 +12,34 @@ export const setKeyword = async (keyword: string, id: number) => {
 
 export const getKeyword = async (id: number) => {
   try {
-    return await redisCli.zRange(`Keyword:${id}`, -5, -1);
+    return await redisCli.sendCommand([
+      'ZRANGE',
+      `keyword:${id}`,
+      '-5',
+      '-1',
+      'REV',
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const countKeyword = async (id: number) => {
+  try {
+    return await redisCli.sendCommand([
+      'ZCOUNT',
+      `keyword:${id}`,
+      '-inf',
+      '+inf',
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const delKeyword = async (id: number) => {
+  try {
+    return await redisCli.sendCommand(['ZMPOPMIN', `keyword:${id}`]);
   } catch (err) {
     console.log(err);
   }
@@ -28,7 +55,51 @@ export const setLike = async (bookId: number, userId: number) => {
 
 export const getLike = async (bookId: number) => {
   try {
-    return redisCli.sMembers(`Like:${bookId}`);
+    return await redisCli.sMembers(`Like:${bookId}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const countLike = async (bookId: number) => {
+  try {
+    return redisCli.sendCommand(['scard', `Like:${bookId}`]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const delLike = async (bookId: number, userId: number) => {
+  try {
+    return await redisCli.sendCommand(['SREM', `Like:${bookId}`, `${userId}`]);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const setView = async (
+  bookId: number,
+  bookItemId: number,
+  userId: number
+) => {
+  try {
+    return await redisCli.sAdd(`view:${bookId}`, `${userId}:${bookItemId}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getView = async (bookId: number) => {
+  try {
+    return await redisCli.sMembers(`view:${bookId}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const countView = async (bookId: number) => {
+  try {
+    return await redisCli.sendCommand(['scard', `view:${bookId}`]);
   } catch (err) {
     console.log(err);
   }
